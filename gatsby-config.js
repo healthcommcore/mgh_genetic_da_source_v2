00853da16435f8dae8509df2e6c938e3aa170fcc
Dave Rothfarb <dave.rothfarb.webdev@gmail.com>
2022-05-09 16:08:56 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `MGH Cancer Genetics Decision Aid`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"]
+};
