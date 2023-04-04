1d2953638db654dad59cc9324b2f92e14393ccec
Khaled Arnaout <khaledarnaout@live.com>
2023-04-04 10:07:54 -0400

    small changes

--- src/components/prev-next-buttons.js
+++ src/components/prev-next-buttons.js
@@ -3,83 +3,88 @@ import PropTypes from "prop-types";
 import NavButton from "./nav-button.js";
 import Container from "react-bootstrap/Container";
 import { Link } from "gatsby";
-import { ucFirst, urlify,ucFirst_es } from "../helpers";
+import { ucFirst, urlify, ucFirst_es } from "../helpers";
 
-const PrevNextButtons = ({ prevNext, isOrphan, advance, retreat, setNewCurrent,lang }) => {
-  const keys = Object.keys(prevNext);
-  const link = {
-    previous: retreat,
-    next: advance
-  }
-  if (lang == 'en'){
-  return (
-    <div className="prev-next-buttons">
-      <Container>
-        <div className="buttons-container d-flex justify-content-around flex-wrap">
-        { isOrphan ?
-          <NavButton 
-            onClick={ () => setNewCurrent(prevNext.current.path) }
-            path={ prevNext.current.path }
-          >{ 'Back to "' + ucFirst(prevNext.current.title) + '"' }</NavButton>
-          :
-          <>
-            { 
-            keys.map( (key, i) => {
-                if (prevNext[key] && key !== "current") {
-                  return (
-                      <NavButton 
-                        key={ i }
-                        className="btn-prev-next"
-                        path={ prevNext[key].path }
-                        onClick={ link[key] }
-                      >
-                        { prevNext[key].title === "End" ? "Finish" : ucFirst(key) }
-                      </NavButton>
-                  );
-                }
-              })}
-          </>
-        }
-        </div>
-      </Container>
-    </div>
-  );}
-  else {
-    return (
-      <div className="prev-next-buttons">
-        <Container>
-          <div className="buttons-container d-flex justify-content-around flex-wrap">
-          { isOrphan ?
-            <NavButton 
-              onClick={ () => setNewCurrent(prevNext.current.path) }
-              path={ prevNext.current.path }
-            >{ 'Back to "' + ucFirst(prevNext.current.title) + '"' }</NavButton>
-            :
-            <>
-              { 
-              keys.map( (key, i) => {
-                  if (prevNext[key] && key !== "current") {
-                    return (
-                        <NavButton 
-                          key={ i }
-                          className="btn-prev-next"
-                          path={ prevNext[key].path }
-                          onClick={ link[key] }
-                        >
+const PrevNextButtons = ({ prevNext, isOrphan, advance, retreat, setNewCurrent, lang }) => {
+    const keys = Object.keys(prevNext);
+    const link = {
+        previous: retreat,
+        next: advance
+    }
+    if (lang == 'en') {
+        return ( <
+            div className = "prev-next-buttons" >
+            <
+            Container >
+            <
+            div className = "buttons-container d-flex justify-content-around flex-wrap" > {
+                isOrphan ?
+                <
+                NavButton
+                onClick = {
+                    () => setNewCurrent(prevNext.current.path) }
+                path = { prevNext.current.path } >
+                { 'Back to "' + ucFirst(prevNext.current.title) + '"' } < /NavButton> :
+                    <
+                    > {
+                        keys.map((key, i) => {
+                            if (prevNext[key] && key !== "current") {
+                                return ( <
+                                    NavButton key = { i }
+                                    className = "btn-prev-next"
+                                    path = { prevNext[key].path }
+                                    onClick = { link[key] } >
+                                    { prevNext[key].title === "End" ? "Finish" : ucFirst(key) } <
+                                    /NavButton>
+                                );
+                            }
+                        })
+                    } <
+                    />
+            } <
+            /div> <
+            /Container> <
+            /div>
+        );
+    } else {
+        return ( <
+            div className = "prev-next-buttons" >
+            <
+            Container >
+            <
+            div className = "buttons-container d-flex justify-content-around flex-wrap" > {
+                isOrphan ?
+                <
+                NavButton
+                onClick = {
+                    () => setNewCurrent(prevNext.current.path) }
+                path = { prevNext.current.path } >
+                { 'Back to "' + ucFirst(prevNext.current.title) + '"' } < /NavButton> :
+                    <
+                    > {
+                        keys.map((key, i) => {
+                            if (prevNext[key] && key !== "current") {
+                                return ( <
+                                    NavButton key = { i }
+                                    className = "btn-prev-next"
+                                    path = { prevNext[key].path }
+                                    onClick = { link[key] } >
 
-                          {
-                          prevNext[key].title === "End" ? "Finalizar" : ucFirst_es(key) }
-                        </NavButton>
-                    );
-                  }
-                })}
-            </>
-          }
-          </div>
-        </Container>
-      </div>
-    );
-  }
+                                    {
+                                        prevNext[key].title === "End" ? "Terminar" : ucFirst_es(key)
+                                    } <
+                                    /NavButton>
+                                );
+                            }
+                        })
+                    } <
+                    />
+            } <
+            /div> <
+            /Container> <
+            /div>
+        );
+    }
 }
 
 export default PrevNextButtons;
